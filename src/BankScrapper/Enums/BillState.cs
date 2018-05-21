80921c54d50654c0d1a6ab8b2c3f8d7bbe19a370
Gabriel Milani <gabrielwmilani@gmail.com>
2018-05-21 20:51:31 -0300

    update nugets

--- src/BankScrapper/Enums/BillState.cs
+++ src/BankScrapper/Enums/BillState.cs
@@ -1,11 +1,18 @@
-﻿namespace BankScrapper.Enums
+﻿using System.ComponentModel;
+
+namespace BankScrapper.Enums
 {
     public enum BillState
     {
+        [Description("N/A")]
         Unknown,
+        [Description("Aberta")]
         Open,
+        [Description("Fechada")]
         Closed,
+        [Description("Atrasada")]
         Overdue,
+        [Description("Paga")]
         Paid
     }
 }
