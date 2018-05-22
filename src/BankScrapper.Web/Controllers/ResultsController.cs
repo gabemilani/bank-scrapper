373956f778984f0b70d32f1078299c9fa1a80127
Gabriel Milani <gabrielwmilani@gmail.com>
2018-05-21 22:31:58 -0300

    Inicio da criação da interface gráfica

---
+++ src/BankScrapper.Web/Controllers/ResultsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace BankScrapper.Web.Controllers
+{
+    [RoutePrefix("resultados")]
+    public class ResultsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/BankScrapper.Web/Scripts/site.js
@@ -0,0 +1,23 @@
+﻿
+$(document).ready(function () {
+
+    $('#bankSelect').change(function () {
+        var value = $('#bankSelect').val();
+        $('#bb-form').css('display', value == 1 ? 'block' : 'none');
+        $('#nubank-form').css('display', value == 2 ? 'block' : 'none');
+        $('#btn-submit').css('display', value == 1 || value == 2 ? 'inline-block' : 'none');
+    });
+
+    $('#btn-submit').click(function () {
+
+        var selectedBank = $('#bankSelect').val();
+        if (selectedBank == 1) {
+
+        } else if (selectedBank == 2) {
+
+        }
+
+
+    });
+
+});
